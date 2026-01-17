6b46cd2dca6d898f7430c4403098ad7aa1a8d3f4
shijin-alpha <shijinthomas369@gmail.com>
2026-01-17 10:34:38 +0530

    Make it extra annoying: 5 scrolling messages at different speeds

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import './App.css'
 function App() {
   return (
     <div className="app">
-      <div className="scrolling-text">
-        VA NOBINEE 😤
-      </div>
+      <div className="scrolling-text scroll-1">VA NOBINEE 😤</div>
+      <div className="scrolling-text scroll-2">VA NOBINEE 😤</div>
+      <div className="scrolling-text scroll-3">VA NOBINEE 😤</div>
+      <div className="scrolling-text scroll-4">VA NOBINEE 😤</div>
+      <div className="scrolling-text scroll-5">VA NOBINEE 😤</div>
     </div>
   )
 }
