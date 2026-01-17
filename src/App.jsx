6c3ac1820584b9d5cde96914cb3a007200cf83fe
shijin-alpha <shijinthomas369@gmail.com>
2026-01-17 10:26:20 +0530

    Initial commit: VA NOBINEE scrolling message

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="scrolling-text">
+        VA NOBINEE 😤
+      </div>
+    </div>
+  )
+}
+
+export default App
