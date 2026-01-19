deb67eb078b28d9a6c0136d4240edd628d0919e0
shijin-alpha <shijinthomas369@gmail.com>
2026-01-19 18:27:21 +0530

    Change to BONI COLOUR CHALLENGE CHEYYAM with rainbow animations

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import './App.css'
 function App() {
   return (
     <div className="app">
-      <div className="scrolling-text scroll-1">VA NOBINEE 😤</div>
-      <div className="scrolling-text scroll-2">VA NOBINEE 😤</div>
-      <div className="scrolling-text scroll-3">VA NOBINEE 😤</div>
-      <div className="scrolling-text scroll-4">VA NOBINEE 😤</div>
-      <div className="scrolling-text scroll-5">VA NOBINEE 😤</div>
+      <div className="scrolling-text scroll-1">BONI COLOUR CHALLENGE CHEYYAM 🎨</div>
+      <div className="scrolling-text scroll-2">BONI COLOUR CHALLENGE CHEYYAM 🌈</div>
+      <div className="scrolling-text scroll-3">BONI COLOUR CHALLENGE CHEYYAM 🎯</div>
+      <div className="scrolling-text scroll-4">BONI COLOUR CHALLENGE CHEYYAM 🔥</div>
+      <div className="scrolling-text scroll-5">BONI COLOUR CHALLENGE CHEYYAM ⚡</div>
     </div>
   )
 }
